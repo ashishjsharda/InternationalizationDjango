6689e6873f202158b0434b1245afb6ef46952caf
Ashish Sharda <ashishjsharda@gmail.com>
2023-01-29 21:38:55 -0600

    update settings

--- internatonalizatonexample/settings.py
+++ internatonalizatonexample/settings.py
@@ -125,6 +125,9 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
+
 USE_TZ = True
 
 
@@ -132,6 +135,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    join(BASE_DIR, 'internatonalizatonexample', "static"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
